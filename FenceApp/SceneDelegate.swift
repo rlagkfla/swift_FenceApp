04762b1b3988a309172423efe2d1413d7bd5adcf
suojae3 <suojae3@gmail.com>
2023-10-19 15:05:51 +0900

    [FEAT]: 이메일 로그인 기능 추가

--- FenceApp/SceneDelegate.swift
+++ FenceApp/SceneDelegate.swift
@@ -6,21 +6,29 @@
 //
 
 import UIKit
+import Firebase
+
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
+    
     let navigationController = UINavigationController()
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         
-        let tabbarController = makeTabbarController()
-        navigationController.viewControllers = [tabbarController]
-        window?.rootViewController = navigationController
+        //        let tabbarController = makeTabbarController()
+        //        navigationController.viewControllers = [tabbarController]
+        //        window?.rootViewController = navigationController
+        //        window?.makeKeyAndVisible()
+        FirebaseApp.configure()
+        let loginVC = LoginViewController()
+        window?.rootViewController = loginVC
         window?.makeKeyAndVisible()
+
+        
     }
     
     func makeTabbarController() -> CustomTabBarController {
@@ -42,7 +50,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let vc = CameraViewController()
         return vc
     }
-
+    
     func makeChatViewController() -> ChatViewController {
         let vc = ChatViewController()
         return vc
@@ -59,27 +67,27 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
-
+    
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
-
+    
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
-
+    
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
     }
-
+    
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
+    
+    
 }
---
+++ FenceApp/Screen/LoginView/LoginViewController/UIExtentsion.swift
@@ -0,0 +1,213 @@
+import UIKit
+import SnapKit
+import RiveRuntime
+import FirebaseAuth
+
+
+
+// MARK: - UIView Extensions
+extension UIView {
+    
+    // MARK: Positioning
+    @discardableResult
+    func centerX() -> Self {
+        guard let superview = self.superview else { return self }
+        snp.makeConstraints { $0.centerX.equalTo(superview) }
+        return self
+    }
+    
+    @discardableResult
+    func centerY() -> Constraint {
+        var constraint: Constraint?
+        guard let superview = self.superview else { return constraint! }
+        snp.makeConstraints {
+            constraint = $0.centerY.equalTo(superview).constraint
+        }
+        return constraint!
+    }
+    
+    @discardableResult
+    func below(_ view: UIView, _ offset: CGFloat) -> Self {
+        snp.makeConstraints { $0.top.equalTo(view.snp.bottom).offset(offset) }
+        return self
+    }
+    
+    @discardableResult
+    func above(_ view: UIView, _ offset: CGFloat) -> Self {
+        snp.makeConstraints { $0.bottom.equalTo(view.snp.top).offset(-offset) }
+        return self
+    }
+    
+    
+    func fullScreen() {
+        guard let superview = self.superview else { return }
+        snp.makeConstraints { $0.edges.equalTo(superview) }
+    }
+    
+    
+    // MARK: Sizing
+    @discardableResult
+    func size(_ width: CGFloat, _ height: CGFloat) -> Self {
+        snp.makeConstraints { $0.width.equalTo(width); $0.height.equalTo(height) }
+        return self
+    }
+    
+    // MARK: Styling
+    @discardableResult
+    func withBlurEffect() -> Self {
+        let blurEffect = UIBlurEffect(style: .light)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = self.bounds
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        blurEffectView.isUserInteractionEnabled = false
+        
+        if let button = self as? UIButton {
+            button.insertSubview(blurEffectView, belowSubview: button.imageView!)
+        } else {
+            insertSubview(blurEffectView, at: 0)
+        }
+        return self
+    }
+
+    @discardableResult
+    func withBorder(color: UIColor, width: CGFloat) -> Self {
+        layer.borderColor = color.cgColor
+        layer.borderWidth = width
+        return self
+    }
+    
+    //MARK: Misc
+    func addSubviews(_ subviews: UIView...) {
+        subviews.forEach { addSubview($0) }
+    }
+    
+    @discardableResult
+    func withBackgroundImage(named imageName: String, at position: CGPoint, size: CGSize? = nil) -> Self {
+        let imageView = UIImageView(image: UIImage(named: imageName))
+        imageView.contentMode = .scaleAspectFit
+        insertSubview(imageView, at: 0)
+        
+        imageView.snp.makeConstraints { make in
+            make.centerX.equalTo(snp.leading).offset(bounds.size.width * position.x)
+            make.centerY.equalTo(snp.top).offset(bounds.size.height * position.y)
+            
+            if let size = size {
+                make.width.equalTo(size.width)
+                make.height.equalTo(size.height)
+            }
+        }
+        
+        return self
+    }
+}
+
+// MARK: - UIButton Extensions
+extension UIButton {
+    
+    @discardableResult
+    func withTitle(_ title: String) -> Self {
+        setTitle(title, for: .normal)
+        return self
+    }
+    
+    @discardableResult
+    func withTextColor(_ color: UIColor) -> Self {
+        setTitleColor(color, for: .normal)
+        return self
+    }
+    
+    @discardableResult
+    func withTarget(_ target: Any?, action: Selector) -> Self {
+        addTarget(target, action: action, for: .touchUpInside)
+        return self
+    }
+    
+    @discardableResult
+    func withBackgroundColor(_ color: UIColor) -> Self {
+        backgroundColor = color
+        return self
+    }
+    
+    @discardableResult
+    func withCornerRadius(_ radius: CGFloat) -> Self {
+        layer.cornerRadius = radius
+        layer.masksToBounds = true
+        return self
+    }
+    
+    @discardableResult
+    func withIcon(named iconName: String, isSystemIcon: Bool = false, pointSize: CGFloat = 20.0, weight: UIImage.SymbolWeight = .regular, scale: UIImage.SymbolScale = .medium, color: UIColor = .black) -> Self {
+        
+        if isSystemIcon {
+            let configuration = UIImage.SymbolConfiguration(pointSize: pointSize, weight: weight, scale: scale)
+            var image = UIImage(systemName: iconName, withConfiguration: configuration)
+            image = image?.withRenderingMode(.alwaysTemplate)
+            setImage(image, for: .normal)
+        } else {
+            var image = UIImage(named: iconName)
+            image = image?.withRenderingMode(.alwaysTemplate)
+            setImage(image, for: .normal)
+        }
+        tintColor = color
+        return self
+    }
+}
+
+// MARK: - UITextField Extensions
+extension UITextField {
+    
+    @discardableResult
+    func withPlaceholder(_ placeholder: String) -> Self {
+        self.placeholder = placeholder
+        return self
+    }
+    
+    @discardableResult
+    func secured() -> Self {
+        isSecureTextEntry = true
+        return self
+    }
+}
+
+// MARK: - UILabel Extensions
+extension UILabel {
+    
+    @discardableResult
+    func withText(_ text: String) -> Self {
+        self.text = text
+        return self
+    }
+    
+    @discardableResult
+    func withFont(_ size: CGFloat, fontName: String = "System") -> Self {
+        if fontName == "System" {
+            font = UIFont.systemFont(ofSize: size)
+        } else {
+            font = UIFont(name: fontName, size: size)
+        }
+        return self
+    }
+
+    @discardableResult
+    func withFontWeight(_ weight: UIFont.Weight) -> Self {
+        font = UIFont.systemFont(ofSize: font.pointSize, weight: weight)
+        return self
+    }
+    
+    @discardableResult
+    func withTextColor(_ color: UIColor) -> Self {
+        self.textColor = color
+        return self
+    }
+    
+}
+
+
+//MARK: - RiveView extension
+extension RiveView {
+    func withViewModel(_ viewModel: RiveViewModel) -> RiveView {
+        isUserInteractionEnabled = false
+        viewModel.setView(self)
+        return self
+    }
+}
