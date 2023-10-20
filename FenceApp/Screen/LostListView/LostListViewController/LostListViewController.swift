0552abeafb8c5ae923f6c72f054df06e5b09af2e
kimhr <khl9566@naver.com>
2023-10-20 12:10:35 +0900

    [ETC] : LostListView 폴더 추가

---
+++ FenceApp/Screen/LostListView/LostListViewController/LostListViewController.swift
@@ -0,0 +1,68 @@
+//
+//  LostListViewController.swift
+//  FenceApp
+//
+//  Created by JeonSangHyeok on 10/16/23.
+//
+
+import UIKit
+import SnapKit
+
+class LostListViewController: UIViewController {
+
+    private let lostListView = LostListView()
+    
+    
+    override func loadView() {
+        view = lostListView
+    }
+    
+    // 임시 버튼
+    lazy var testBtnnn: UIButton = {
+        let btn = UIButton()
+        btn.setImage(UIImage(systemName: "plus"), for: .normal)
+        btn.addTarget(self, action: #selector(tapPlusBtns), for: .touchUpInside)
+        return btn
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.backgroundColor = .white
+//        self.navigationController?.navigationItem.title = "test"
+//        self.navigationItem.title = "test"
+        view.addSubview(testBtnnn)
+        
+        testBtnnn.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(100)
+            $0.trailing.equalToSuperview().offset(-13)
+        }
+    }
+    
+    @objc func tapPlusBtns(){
+        let enrollVC = EnrollViewController()
+        
+        self.navigationController?.pushViewController(enrollVC, animated: true)
+    }
+//    override func viewWillAppear(_ animated: Bool) {
+//        navigationController?.isNavigationBarHidden = false
+//    }
+
+//    func setupNaviBar() {
+//        title = "test11"
+//        // (네비게이션바 설정관련) iOS버전 업데이트 되면서 바뀐 설정:별:️:별:️:별:️
+//        let appearance = UINavigationBarAppearance()
+//        appearance.configureWithOpaqueBackground() // 불투명으로
+//       // appearance.backgroundColor = .black // bartintcolor가 15버전부터 appearance로 설정하게끔 바뀜
+//        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.orange]
+//        appearance.titleTextAttributes = [.foregroundColor: UIColor.orange]
+//        appearance.shadowColor = .clear
+//        self.navigationController?.navigationBar.prefersLargeTitles = true
+//        self.navigationController?.navigationBar.tintColor = .orange
+//        self.navigationController?.navigationBar.standardAppearance = appearance
+//        self.navigationController?.navigationBar.compactAppearance = appearance
+//        self.navigationController?.navigationBar.scrollEdgeAppearance = appearance
+//        self.navigationItem.hidesSearchBarWhenScrolling = false
+//    }
+
+}
