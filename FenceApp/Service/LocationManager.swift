622d21bf47c433e558a0c1303d317c45df6e11ba
suojae3 <suojae3@gmail.com>
2023-11-05 17:11:45 +0900

    [FEAT]: 로그인뷰컨 locationmanager 생성자 추가

--- FenceApp/Service/LocationManager.swift
+++ FenceApp/Service/LocationManager.swift
@@ -70,7 +70,4 @@ class LocationManager: NSObject, CLLocationManagerDelegate {
                 print("GPS: Default")
             }
         }
-    // sceneDelegate user가 로그인 되어있으면서 위치 공유도 되어있으면 탭바, 아니면 로그인뷰,
-    // loginView 들어오면(location manager)   -> location 셋팅 되있나 확인하고 -> 안되있으면 alert창 "무조건 승인해야함" 설정으로 보내? -> 로그인뷰컨트롤러 상태 -> 로그인-> location 세팅되있나 확인하 안되면 alert창 뛰우고 설정으로 보냄 ->
-    // location.status == false alert하나 띄어서 너이것안하면 못쓴다 확인누르면 설정으로 보내고,
 }
