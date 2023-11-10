c112b21608b6649c6fc701c84e7630c2138aac92
suojae3 <suojae3@gmail.com>
2023-11-10 22:18:44 +0900

    [FIX]: 중복코드 삭제

--- FenceApp/Screen/LoginView/LoginViewController/ColorHandler.swift
+++ FenceApp/Screen/LoginView/LoginViewController/ColorHandler.swift
@@ -24,25 +24,3 @@ class ColorHandler {
 
 
 
-import UIKit
-
-
-class ColorHandler {
-    static let shared = ColorHandler()
-    
-    let titleColor: UIColor
-    let textColor: UIColor
-    let buttonTextColor: UIColor
-    let buttonActivatedColor: UIColor
-    let buttonDeactivateColor: UIColor
-    
-    
-    private init() {
-        titleColor = UIColor(hexCode: "55BCEF")
-        textColor =  UIColor(hexCode: "55BCEF")
-        buttonTextColor = .white
-        buttonActivatedColor = UIColor(hexCode: "55BCEF")
-        buttonDeactivateColor = UIColor(hexCode: "A9A9A9")
-    }
-}
-
